2cf35d685648bb3b7ff24ef6ef310d3c4e52a65b
root <root@ec2-13-59-151-67.us-east-2.compute.amazonaws.com>
2017-06-27 16:37:25 +0000

    fix script names

--- bin/install_baseline.sh
+++ bin/install_baseline.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 
-cp -r config/discoverFile /opt/phoenix/config
-chown -R admin:admin /opt/phoenix/config/discoverFile
-sed -i.orig 's/^output_cmdbupdate_to_file.*/output_cmdbupdate_to_file=1 #0: no 1: yes/' /opt/phoenix/config/phoenix_config.txt
-sed -i 's/^discover_from_file.*/discover_from_file=1 #0:from network; 1:from file/' /opt/phoenix/config/phoenix_config.txt
