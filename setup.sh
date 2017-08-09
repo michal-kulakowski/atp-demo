f5e85d396bec7a0f3554666df872dcb6662dd886
Michał Kułakowski <michal@wloczykije.info>
2017-08-09 17:26:23 +0200

    Eliminate need for restart in setup, documentation improvements

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo -n "Installing Discovery Data: "
 bin/install_discovery_data.sh
+echo "DONE"
+echo -n "Configuring mail Server: "
 bin/install_mail_server.sh
-reboot
+echo "DONE"
+echo -n "Restarting Services: "
+phtools --stop ALL && phtools --stop phMonitor > /dev/null 2>&1 &
+while [ `/opt/phoenix/bin/phstatus.py | grep DOWN | wc | awk '{print $1}'` != "0" ]; do
+	echo -n ".";
+	sleep 1;
+done
+echo "DONE"
