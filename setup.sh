f2a8576620c54a0c9d0193573440db2a489f76a5
Michał Kułakowski <michal@wloczykije.info>
2017-08-09 17:43:35 +0200

    fixes

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo -n "Installing Discovery Data: "
-bin/install_discovery_data.sh > ./setup.log 2>&1 &
+echo -n "Installing discovery data: "
+bin/install_discovery_data.sh > ./setup.log 2>&1
 echo "DONE"
-echo -n "Configuring mail Server: "
-bin/install_mail_server.sh > ./setup.log 2>&1 &
+echo -n "Configuring mail server: "
+bin/install_mail_server.sh > ./setup.log 2>&1
 echo "DONE"
-echo -n "Restarting Services: "
-phtools --stop ALL > ./setup.log 2>&1 &
-phtools --stop phMonitor > ./setup.log 2>&1 &
+echo -n "Restarting Services: ."
+phtools --stop ALL > ./setup.log 2>&1
+phtools --stop phMonitor > ./setup.log 2>&1
 while [ `/opt/phoenix/bin/phstatus.py | grep DOWN | wc | awk '{print $1}'` != "0" ]; do
 	echo -n ".";
 	sleep 1;
