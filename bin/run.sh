4851ead6c4a97f03f185ad5b54d7cdf8e0d3097b
Michał Kułakowski <michal@wloczykije.info>
2017-08-13 14:40:32 +0200

    add DHCP information to run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+bin/sendEvents -r 1 -d 127.0.0.1 -m config/events/0_host_registered -l 1
+sleep 20
 bin/sendEvents -r 1 -d 127.0.0.1 -m config/events/1_malware_downloaded -l 1
 sleep 20
 bin/sendEvents -r 1 -d 127.0.0.1 -m config/events/2_smb_outbreak -l 1
