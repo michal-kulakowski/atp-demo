9564f7e6cfcee0d06f898a3564cab0959a1ff709
Michał Kułakowski <michal@wloczykije.info>
2017-08-13 11:14:59 +0200

    directionality fix - log

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo "DONE"
 echo -n "Configuring mail server: "
 bin/install_mail_server.sh > ./setup.log 2>&1
 echo "DONE"
-echo -n "Restarting Services: ."
+echo -n "Restarting services: ."
 phtools --stop ALL > ./setup.log 2>&1
 phtools --stop phMonitor > ./setup.log 2>&1
 while [ `/opt/phoenix/bin/phstatus.py | grep DOWN | wc | awk '{print $1}'` != "0" ]; do
